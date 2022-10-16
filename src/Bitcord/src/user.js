0b1b82a84c24fc5c988876872ed260a40156e3cd
zachweinstein <zachary.r.weinstein@gmail.com>
2022-10-16 01:08:29 -0700

    added more stuff

--- src/Bitcord/src/user.js
+++ src/Bitcord/src/user.js
@@ -1,13 +1,16 @@
 var user;
+var hubs;
 
-function create_user(alias, pass, cb, opt){
-    gun.get((alias, pass)).put({name: alias});
+function create_user(){
+    var alias = document.getElementById('username').value;
+    var pass = document.getElementById('password').value;
+    gun.get('users').set(user)
 }
 
-function login(event){
-    gun.get((alias, pass));
+function login(){
+    var alias = document.getElementById('username').value;
+    var pass = document.getElementById('password').value;
+    // TODO - throw an error if the user does not exist
+    user = gun.get((alias, pass));
+    console.log(user.name);
 }
-
-const login = document.getElementById('login');
-
-login.addEventListener('submit', login);
