9fe96dae181cad4f9d1b0b1a48abb7d7ec31670d
zachweinstein <zachary.r.weinstein@gmail.com>
2022-10-16 03:20:28 -0700

    figured out gun

--- src/Bitcord/src/user.js
+++ src/Bitcord/src/user.js
@@ -1,16 +1,28 @@
-var user;
-var hubs;
+
+gun.put('users');
+const users = gun.get('users');
 
 function create_user(){
     var alias = document.getElementById('username').value;
     var pass = document.getElementById('password').value;
-    gun.get('users').set(user)
+    var food = document.getElementById('favoritefood').value;
+
+    var current_user = {alias: alias, password: pass, food: food, own_hubs, hubs};
+    users.put(alias + pass)
+    current_user = users.get(alias + pass).put(current_user);
+
+    current_user.on((data) =>{
+        console.log(data);
+    })
 }
+
 
 function login(){
     var alias = document.getElementById('username').value;
     var pass = document.getElementById('password').value;
-    // TODO - throw an error if the user does not exist
-    user = gun.get((alias, pass));
-    console.log(user.name);
+    current_user = users.get(alias + pass);
+    current_user.on((data) =>{
+        console.log(data);
+    });
+    // TODO - login system
 }
