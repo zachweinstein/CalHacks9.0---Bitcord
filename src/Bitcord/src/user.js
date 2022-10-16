b5bb5b10a6f74fe7a217ec1a0d7c6d03886ec1be
zachweinstein <zachary.r.weinstein@gmail.com>
2022-10-16 05:45:03 -0700

    added basic ability to store messages persistently

--- src/Bitcord/src/user.js
+++ src/Bitcord/src/user.js
@@ -1,16 +1,15 @@
-
-gun.put('users');
 const users = gun.get('users');
-
 var current_user;
+
+function Message(message){
+    this.message = message;
+}
 
 function create_user(){
     var alias = document.getElementById('username').value;
     var pass = document.getElementById('password').value;
-    var food = document.getElementById('favoritefood').value;
-    var secretKey = Uint8Array.from(generate_key());
-    current_user = {alias: alias, password: pass, secretkey: };
-    users.put(alias + pass)
+    current_user = {alias: alias, password: pass, messages:{}};
+    users.put(alias + pass);
     current_user = users.get(alias + pass).put(current_user);
 
     current_user.on((data) =>{
@@ -25,12 +24,13 @@ function login(){
     current_user.on((data) =>{
         console.log(data);
     });
+    loggedin = true;
 }
 
-function generate_key(){
-    var key = Array(64);
-    for(let i = 0; i < key.length; i++){
-        key[i] = Math.floor(Math.random * 255 + 1);
-    }
-    return key;
+function send_message(){
+    var message = new Message(document.getElementById('message').value);
+    current_user.get('messages').put(message);
+    current_user.get('messages').on((data) =>{
+        console.log(data);
+    });
 }
